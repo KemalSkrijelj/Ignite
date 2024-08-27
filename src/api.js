af37936635c8f031ecceb779f5b91ceb987aed7b
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-08-27 20:18:02 +0200

    Bug fixed

--- src/api.js
+++ src/api.js
@@ -1,26 +1,18 @@
 //Base URL
-const base_url = "https://api.rawg.io/api/";
+const base_url = "https://api.rawg.io/api";
+const apiKey = "c84e397231ed4407ab608fc978756728";
 
 //Getting the date
 const getCurrentMonth = () => {
   const month = new Date().getMonth() + 1;
-  if (month < 10) {
-    return `0${month}`;
-  } else {
-    return month;
-  }
-};
-//Getting the date
-const getCurrentDay = () => {
-  const day = new Date().getDate();
-  if (day < 10) {
-    return `0${day}`;
-  } else {
-    return day;
-  }
+  return month < 10 ? `0${month}` : month;
 };
 
-//Current day/month/year
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  return day < 10 ? `0${day}` : day;
+};
+
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
@@ -28,11 +20,10 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
-//Popular Games
-const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${apiKey}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
-export const popularGamesURL = () => `${base_url}${popular_games}`;
-export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
-export const newGamesURL = () => `${base_url}${new_games}`;
+export const popularGamesURL = () => `${base_url}/${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}/${upcoming_games}`;
+export const newGamesURL = () => `${base_url}/${new_games}`;
